b4e7521c000ff1da7c01f197ff73d24ab283d099
daniel-umaojo <danielumaojo@gmail.com>
2022-06-14 08:45:04 -0700

    New

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (success)
+*/
 #include <string.h>
 int main(void)
 {
-	char str1[51];
+	char str1[52];
 
 	strcpy(str1, "Programming is like building a multilingual puzzle");
 
