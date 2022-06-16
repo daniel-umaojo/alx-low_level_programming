f68de28e85c43cd45bd4abf90f6f19f2b3926a19
daniel-ameh <danielumaojo@gmail.com>
2022-06-16 21:49:39 +0100

    Update 4-print_alphabt.c
    New

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- * main- Entry point
- * Description:C file that reads alphabet
- * Return:Always 0 (success)
-*/
-int main(void)
-{
-	char char_filter(char c)
-  {
-    if 
-  char c;
-
-	for (c = 'a'; c <= 'z'; c != q; c !=e; c++)
-		putchar(c);
-	return (0);
-}
+#include <stdio.h>                                                                                                                    
+#include <stdlib.h>                                                                                                                   
+#include <time.h>                                                                                                                     
+/**                                                                                                                                   
+ * main- Entry point                                                                                                                  
+ * Description:C file that reads alphabet                                                                                             
+ * Return:Always 0 (success)                                                                                                          
+*/                                                                                                                                    
+int main(void)                                                                                                                        
+{                                                                                                                                     
+        char c;                                                                                                                                                                                                                                       
+                                                                                                                                      
+        for (c = 'a'; c <= 'z'; c != 'e'; c != q; c++)                                                                                                  
+                putchar(c);                                                                                                                                                                                                                 
+        putchar('\n');                                                                                                                
+        return (0);                                                                                                                   
+}                   
