131c45abe52bf0b1e42f2500f6cfc4f0d4a5db3f
daniel-ameh <danielumaojo@gmail.com>
2022-06-16 21:55:55 +0100

    Create 5-print_numbers.c
    New

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>                                                                                                                    
+#include <stdlib.h>                                                                                                                   
+#include <time.h>                                                                                                                     
+/**                                                                                                                                   
+ * main- Entry point                                                                                                                  
+ * Description:C file that reads alphabet                                                                                             
+ * Return:Always 0 (success)                                                                                                          
+*/                                                                                                                                    
+int main(void)                                                                                                                        
+{                                                                                                                                     
+        char c;
+  
+        for (c = 0; c <= 9; c++)                                                                                                  
+                putchar(c);                                                                                                  
+        putchar('\n');                                                                                                                
+        return (0);                                                                                                                   
+}                   
