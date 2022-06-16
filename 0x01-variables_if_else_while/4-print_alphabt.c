1658803d48f975b8d9f519e1dbb386e80cb97124
daniel-umaojo <danielumaojo@gmail.com>
2022-06-16 15:14:11 -0700

    new

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,17 @@
-#include <stdio.h>                                                                                                                    
-#include <stdlib.h>                                                                                                                   
-#include <time.h>                                                                                                                     
-/**                                                                                                                                   
- * main- Entry point                                                                                                                  
- * Description:C file that reads alphabet                                                                                             
- * Return:Always 0 (success)                                                                                                          
-*/                                                                                                                                    
-int main(void)                                                                                                                        
-{                                                                                                                                     
-        char c;                                                                                                                                                                                                                                       
-                                                                                                                                      
-        for (c = 'a'; c <= 'z'; c != 'e'; c != q; c++)                                                                                                  
-                putchar(c);                                                                                                                                                                                                                 
-        putchar('\n');                                                                                                                
-        return (0);                                                                                                                   
-}                   
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main- Entry point
+ * Description:C file that reads alphabet
+ * Return:Always 0 (success)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c != 'e'), ( c != 'q' c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
