bde194b69a6385878bff56c9408881291dff46b3
daniel-ameh <danielumaojo@gmail.com>
2022-06-16 21:46:48 +0100

    Create 4-print_alphabt.c
    new

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main- Entry point
+ * Description:C file that reads alphabet
+ * Return:Always 0 (success)
+*/
+int main(void)
+{
+	char char_filter(char c)
+  {
+    if 
+  char c;
+
+	for (c = 'a'; c <= 'z'; c != q; c !=e; c++)
+		putchar(c);
+	return (0);
+}
