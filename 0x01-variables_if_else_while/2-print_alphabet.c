0bdf385a282b898f8577fa5e83b6536e169d381c
daniel-ameh <danielumaojo@gmail.com>
2022-06-16 21:28:49 +0100

    Create 2-print_alphabet.c
    New

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main- Entry point
+ * Description:C file that reads alphabet
+ * Return:Always 0 (success)
+*/
+int main(void)
+{
+	char c;
+	char d;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+  putchar('\n');
+	return (0);
+}
